83b7356d2b0ee3293ee105b4697bc9d523b9264f
TheDoru <thedoruv2@gmail.com>
2022-12-10 17:42:57 +0100

    passport config for authentication

--- app.js
+++ app.js
@@ -6,14 +6,47 @@ const LocalStrategy = require("passport-local");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bcrypt = require("bcryptjs");
 
 var indexRouter = require("./routes/index");
+
+const User = require("./models/user");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+passport.use(
+  new LocalStrategy((email, password, done) => {
+    User.findOne({ email: email }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect user" });
+      }
+      bcrypt.compare(password, user.password, (err, success) => {
+        if (user.password !== success) {
+          return done(err, false, { message: "Incorrect password" });
+        } else {
+          return done(null, user);
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 app.use(
   session({
