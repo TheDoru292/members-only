2b263809140d71672c539743d0f403ebd5aaf024
TheDoru <thedoruv2@gmail.com>
2022-12-10 17:42:37 +0100

    mongoose connection and route for sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const mongoDb = process.env.MONGODB_LINK;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", (error) => console.bind.error(error));
+
+const user = require("../controller/userController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/sign-up", user.signup_get);
+
+router.post("/sign-up", user.signup_post);
 
 module.exports = router;
