ac502fb245048503646521d900bfef19889cb1fc
thedoru <thedoruv2@gmail.com>
2022-12-13 17:38:38 +0100

    renamed message.js to post.js

--- models/post.js
+++ models/post.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
+const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   text: { type: String, required: true },
   timestamp: Date,
 });
 
-module.exports = mongoose.model("Message", MessageSchema);
+module.exports = mongoose.model("Message", PostSchema);
