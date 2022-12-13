75d8a20eafaa9879155849c9ffd99e8199c91971
thedoru <thedoruv2@gmail.com>
2022-12-13 17:41:16 +0100

    routes for create post page

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.bind.error(error));
 
 const user = require("../controller/userController");
+const post = require("../controller/postController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -33,5 +34,9 @@ router.post("/join-the-club", user.join_the_club_post);
 router.get("/admin", user.admin_get);
 
 router.post("/admin", user.admin_post);
+
+router.get("/create-post", post.create_post_get);
+
+router.post("/create-post", post.create_post);
 
 module.exports = router;
